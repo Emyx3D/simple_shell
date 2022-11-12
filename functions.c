a6a39bf98f1c0bcca4bcab922df01922d0129b96
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-11-12 17:04:24 +0100

    4 functions put in one file

--- functions.c
+++ functions.c
@@ -29,10 +29,40 @@ char *_strcpy(char *dest, const char *src)
 {
 	int i = -1;
 
-	do {
+	do
+	{
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
 
 	return (dest);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - prints a string
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
--- stringToTokens.c
+++ stringToTokens.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * *nbr_spaces - return the number of occurent of a string
@@ -12,42 +12,41 @@ unsigned int nbr_spaces(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i]  == ' ')
+		if (s[i] == ' ')
 			cmpt++;
 	}
 
 	return (cmpt);
 }
 
-
 /**
-  *stringToTokens - split a sentence into multiple words.
-  *@str: the string passed as argument.
-  *Return: tokens
-  */
+ *stringToTokens - split a sentence into multiple words.
+ *@str: the string passed as argument.
+ *Return: tokens
+ */
 char **stringToTokens(char *str)
 {
-int i = 0;
-const char separator[] = " ";
-int spaces = nbr_spaces(str);
-char **tokens = malloc(sizeof(char *) * (spaces + 1));
-char *token;
+	int i = 0;
+	const char separator[] = " ";
+	int spaces = nbr_spaces(str);
+	char **tokens = malloc(sizeof(char *) * (spaces + 1));
+	char *token;
 
-if (!tokens)
-{
-	fprintf(stderr, "sh: allocation error\n");
-	exit(1);
-}
+	if (!tokens)
+	{
+		fprintf(stderr, "sh: allocation error\n");
+		exit(1);
+	}
 
-token = strtok(str, separator);
+	token = strtok(str, separator);
 
-while (token != NULL)
-{
-	tokens[i] = token;
-	token = strtok(NULL, separator);
-	i++;
-}
-tokens[i] = NULL;
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, separator);
+		i++;
+	}
+	tokens[i] = NULL;
 
-return (tokens);
+	return (tokens);
 }
