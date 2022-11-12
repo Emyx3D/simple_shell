7045969c05b1aef9dccd8bd89af87d3ad75a1d66
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-11-12 22:27:13 +0100

    several changes

--- shell_0.4.c
+++ shell_0.4.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "main.h"
 #include <sys/stat.h>
 
 /**
@@ -17,45 +12,45 @@
 
 void read_command(char cmd[], char *par[])
 {
-        char *line;
-        size_t buflen = 64;
-        int chars;
-        int count = 0, i = 0, j = 0;
-        char *pch;
-        char *array[100];
+	char line[1024];
+	int count = 0, i = 0, j = 0;
+	char *pch;
+	char *array[100];
 
-        /*reads one line*/
-        line = (char *)malloc(buflen * sizeof(char));
-        if (line == NULL)
-        {
-                perror("unable to allocate memory...");
-                exit(1);
-        }
-        chars = getline(&line, &buflen, stdin);
-        line[count++] = (char)chars;
+	/*reads one line*/
+	for (;;)
+	{
+		int c = fgetc(stdin);
 
-        if (count == 1)
-        {
-                return;
-        }
+		line[count++] = (char)c;
+		if (c == '\n')
+		{
+			return;
+		}
+	}
 
-        /*parse the line into words*/
-        pch = strtok(line, " \n");
-        while (pch != NULL)
-        {
-                array[i++] = strdup(pch);
-                pch = strtok(NULL, " \n");
-        }
+	if (count == 1)
+	{
+		return;
+	}
 
-        /*first word is the command*/
-        strcpy(cmd, array[0]);
+	/*parse the line into words*/
+	pch = strtok(line, " \n");
+	while (pch != NULL)
+	{
+		array[i++] = strdup(pch);
+		pch = strtok(NULL, " \n");
+	}
 
-        /*others are parameters*/
-        for (j = 0; j < i; j++)
-        {
-                par[j] = array[j];
-        }
-        par[i] = NULL;
+	/*first word is the command*/
+	strcpy(cmd, array[0]);
+
+	/*others are parameters*/
+	for (j = 0; j < i; j++)
+	{
+		par[j] = array[j];
+	}
+	par[i] = NULL;  /*null terminating the parameter list*/
 }
 
 /**
@@ -66,13 +61,7 @@ void read_command(char cmd[], char *par[])
 
 void the_prompt(void)
 {
-        int first_time = 1;
-
-        while (first_time) /*prompts an infinite loop for the user to enter commands*/
-        {
-                write(STDOUT_FILENO, "#cisfun$ ", 10);
-                first_time = 0;
-        }
+	printf("#cisfun$ ");
 }
 
 /**
@@ -83,32 +72,32 @@ void the_prompt(void)
  */
 int main(int argc, char *argv[])
 {
-        char cmd[1000];
-        char command[1000];
-        char *parameters[20];
+	char cmd[1000];
+	char command[1000];
+	char *para[20];
 
-        /*creating environment variable*/
-        char *envp[] = {(char *)"PATH=/usr/bin/", NULL};
+	/*creating environment variable*/
+	char *envp[] = {(char *)"PATH=/bin", NULL};
 
-        while (1)
-        {                                          /*repeat forever*/
-                the_prompt();                      /*display prompt on screen*/
-                read_command(command, parameters); /*read input from terminal*/
-                if (fork() != 0)                   /*parent*/
-                {
-                        wait(NULL); /*wait for child precesses to finish*/
-                }
-                else
-                {
-                        strcpy(cmd, "/usr/bin/");
-                        strcat(cmd, command);
-                        execve(cmd, parameters, envp); /*execute command*/
-                }
+	while (1)
+	{					   /*repeat forever*/
+		the_prompt();			   /*display prompt on screen*/
+		read_command(command, para); /*read input from terminal*/
+		if (fork() != 0)		   /*parent*/
+		{
+			wait(NULL); /*wait for child precesses to finish*/
+		}
+		else
+		{
+			strcpy(cmd, "/bin/");
+			strcat(cmd, command);
+			execve(cmd, para, envp); /*execute command*/
+		}
 
-                if (strcmp(cmd, "exit") == 0)
-                {
-                        break;
-                }
-        }
-        return (0);
+		if (strcmp(command, "exit") == 0)
+		{
+			break;
+		}
+	}
+	return (0);
 }
